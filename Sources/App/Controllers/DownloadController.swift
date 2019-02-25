4fa8dbc5cfd1081382ced8ac2c6023867199e06f
Gudmunduro <goh140400@gmail.com>
2019-02-25 19:39:41 +0000

    Removed error msg

--- Sources/App/Controllers/DownloadController.swift
+++ Sources/App/Controllers/DownloadController.swift
@@ -11,21 +11,13 @@ final class DownlaodSiteController {
     func download(_ req: Request) throws -> Future<Response>
     {
         let fileTag = try req.parameters.next(String.self)
-        let dbData = DownloadFile.query(on: req).filter(\.fileTag == fileTag).first()
-        dbData.catch { (error) throws -> () in
-            print(error)
-            throw Abort(.badRequest, reason: "File does not exist")
-        }
-        return dbData.map { fileData -> Response in
+        return DownloadFile.query(on: req).filter(\.fileTag == fileTag).first().map { fileData in
             guard fileData != nil else {
-                print("Filedata is nil")
                 throw Abort(.badRequest, reason: "File does not exist")
             }
             guard let data = try? Data(contentsOf: fileStoragePath(filename: fileData!.fileTag)) else {
-                print("Failed to load data")
                 throw Abort(.internalServerError, reason: "Failed to load data")
             }
-            print("File exists")
             let file = File(data: data, filename: fileData!.filename)
             return req.response(file: file)
         }
